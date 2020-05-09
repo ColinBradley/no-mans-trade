74b064d8c5845a3579760502515f1850834850eb
Colin Bradley <colin@colinbradley.co.uk>
2020-05-09 18:04:13 +0100

    ObservableProperty - now only calls BeginInvoke when required. There was no real reason to make this change other than it bothered me that it didn't

--- src/NoMansTrade.App/Support/ObservableProperty.cs
+++ src/NoMansTrade.App/Support/ObservableProperty.cs
@@ -33,8 +33,16 @@ namespace NoMansTrade.App.Support
 
                 mValue = value;
 
-                mDispatcher.BeginInvoke(
-                    () => this.PropertyChanged?.Invoke(this, sValueArgs));
+                if (mDispatcher.CheckAccess())
+                {
+                    this.PropertyChanged?.Invoke(this, sValueArgs);
+                }
+                else
+                {
+                    mDispatcher.BeginInvoke(
+                        () => this.PropertyChanged?.Invoke(this, sValueArgs));
+                }
+
             }
         }
     }
